17c6d5d340f0f5ee11fce9700357c626a7c6c5de
szepesivirag <szgvirag54@gmail.com>
2020-10-11 16:05:28 +0200

    Kezdo file-ok

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+
+void szamkitalalo()
+{
+    int kitalalando, bemenet;
+
+    kitalalando = rand() % 100 + 1;
+    bemenet = 0;
+
+    printf("Indul a talalgatas 1 es 100 kozott \n");
+
+    while(kitalalando != bemenet){
+        printf("Tippelj: ");
+        scanf("%d", &bemenet);
+
+        if(kitalalando < bemenet)
+        {
+            printf("Nem jo, tippelj kisebbet\n");
+        }
+        if(kitalalando > bemenet)
+        {
+            printf("Nem jo, tippelj nagyobbat\n");
+        }
+    }
+    printf("Talalt!\n");
+}
+
+int main(void)
+{
+	char igen_nem;
+
+    srand(time(0));
+
+    while (1)
+    {
+        szamkitalalo();
+
+        printf("Jatszunk tovabb? [i/n]");
+        scanf("%s", &igen_nem);
+        if(igen_nem != 'i')
+            break;
+    }
+    return 0;
+}
